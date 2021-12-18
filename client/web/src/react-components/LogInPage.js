225a271c33d083b8ed64a2488cedbdf89785f3cc
Roman Ojha <razzroman98@gmail.com>
2021-12-18 17:38:21 +0545

    designing signIn page

---
+++ client/web/src/react-components/LogInPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import App_Icon from "../images/App_Icon.png";
+import Facebook_Icon from "../images/facebook_icon.svg";
+import Google_Icon from "../images/google_icon.svg";
+import Github_Icon from "../images/github_icon.svg";
+
+const LogInPage = () => {
+  return (
+    <>
+      <div className="LoginPage">
+        <div className="LoginPage_Container">
+          <img src={App_Icon} alt="Logo" className="LoginPage_App_Logo" />
+          <h1 className="LoginPage_SignIn_Title">Sign In</h1>
+          <div className="LoginPage_Button_Container">
+            <div>
+              <img src={Github_Icon} />
+              <h2>GitHub</h2>
+            </div>
+            <div>
+              <img src={Facebook_Icon} />
+              <h2 style={{ color: "#1877F2" }}>Facebook</h2>
+            </div>
+            <div>
+              <img src={Google_Icon} />
+              <h2 style={{ color: "#EF6054" }}>Google</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LogInPage;
