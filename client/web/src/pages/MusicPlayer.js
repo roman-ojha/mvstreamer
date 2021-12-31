a30368c0e2125cad0147975785e592dce6f5618d
Roman Ojha <razzroman98@gmail.com>
2021-12-31 15:34:48 +0545

    completing positining the react music palyer element

--- client/web/src/pages/MusicPlayer.js
+++ client/web/src/pages/MusicPlayer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import User_Image from "../assets/images/user.jpg";
-import Song_Image01 from "../assets/images/carousel_Image_01.jpg";
+import Song_Image01 from "../assets/images/carousel_Image_02.jpg";
 import Song_Image02 from "../assets/images/carousel_Image_01.jpg";
-import Song_Image03 from "../assets/images/carousel_Image_01.jpg";
+import Song_Image03 from "../assets/images/carousel_Image_03.jpg";
+import Background_Image from "../assets/svg/Music_Player_Background.svg";
 
 const VolumeController = () => {
-  return <></>;
+  return (
+    <>
+      <div className="Music_Player_Volume_Controller">
+        <div className="Music_Player_Volume_Controller_Button"></div>
+        <div className="Music_Player_Current_Volume"></div>
+      </div>
+    </>
+  );
 };
 
 const ProgressBarController = () => {
@@ -16,37 +24,66 @@ const ProgressBarController = () => {
 const MusicPlayer = () => {
   return (
     <>
-      <div className="Music_Player_Background">
-        <div className="Music_Player_Container">
-          <div className="Music_Player_BackButton_And_Profile">
-            <Icon icon="eva:arrow-ios-back-outline" />
-            <img src={User_Image} />
-          </div>
-          <div className="Music_Player_Volume_And_Song_Image_Container">
-            <VolumeController />
-            <img src={Song_Image01} />
-            <img src={Song_Image02} />
-            <img src={Song_Image03} />
-            <VolumeController />
-          </div>
-          <div className="Music_Player_Song_Title_Container">
-            <h2>Kavhi Khusi Kavhi Gam</h2>
-            <p>Sonu Nigam</p>
-          </div>
-          <div className="Music_Player_TimeStamp_ProgressBar_Container">
+      <div className="Music_Player_Background"></div>
+      <div className="Music_Player_Container">
+        <div className="Music_Player_BackButton_And_Profile">
+          <Icon width="60px" icon="eva:arrow-ios-back-outline" />
+          <img src={User_Image} />
+        </div>
+        <div className="Music_Player_Volume_And_Song_Image_Container">
+          <VolumeController />
+          <img className="Music_Player_Song_Small_Image" src={Song_Image01} />
+          <img className="Music_Player_Big_Small_Image" src={Song_Image02} />
+          <img className="Music_Player_Song_Small_Image" src={Song_Image03} />
+          <VolumeController />
+        </div>
+        <div className="Music_Player_Song_Title_Container">
+          <h2>Kavhi Khusi Kavhi Gam</h2>
+          <p>Sonu Nigam</p>
+        </div>
+        {/* <div className="Music_Player_TimeStamp_ProgressBar_Container">
             <p>1:45</p>
             <ProgressBarController />
             <p>3:20</p>
-          </div>
-          <div>
-            <Icon icon="cil:loop-1" />
-            <Icon icon="ic:round-favorite" />
-            <Icon icon="fluent:previous-24-filled" />
-            <Icon icon="bi:play-circle-fill" />
-            <Icon icon="fluent:next-24-filled" />
-            <Icon icon="bx:bxs-playlist" />
-            <Icon icon="fa-solid:random" />
-          </div>
+          </div> */}
+        <div className="Music_Player_Pause_Play_Next_Previous_Button_Controller">
+          <div></div>
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="50px"
+            icon="cil:loop-1"
+          />
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="50px"
+            icon="ic:round-favorite"
+          />
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="50px"
+            icon="fluent:previous-24-filled"
+          />
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="70px"
+            icon="bi:play-circle-fill"
+          />
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="50px"
+            icon="fluent:next-24-filled"
+          />
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="50px"
+            icon="bx:bxs-playlist"
+          />
+          <Icon
+            // style={{ margin: "0px 5px 0px 5px" }}
+            width="50px"
+            icon="fa-solid:random"
+          />
+          <div></div>
         </div>
       </div>
     </>
