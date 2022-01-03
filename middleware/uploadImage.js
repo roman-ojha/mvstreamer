3365d09cba88f7503e3ba557315089e32c57f6fc
Roman Ojha <razzroman98@gmail.com>
2022-01-03 10:49:30 +0545

    uploading image from client to server

---
+++ middleware/uploadImage.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+import crypto from "crypto";
+import path from "path";
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    crypto.randomBytes(16, (err, buf) => {
+      const filename = buf.toString("hex") + path.extname(file.originalname);
+      cb(null, filename);
+    });
+  },
+  destination: "../db/Images",
+});
+
+export default multer({ storage });
