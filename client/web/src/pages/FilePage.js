dad486bd16d284575a36e5a4f7627e9614155ac1
Roman Ojha <razzroman98@gmail.com>
2022-01-13 22:46:53 +0545

    showing selected folder in file/local page in react

--- client/web/src/pages/FilePage.js
+++ client/web/src/pages/FilePage.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from '@iconify/react';
 
 const FilePage = () => {
-  const Folder=()=>{
+  const [rootFolders,setRootFolder]=useState(["Picture","Image","Studio","Wallpapers","Music","Video"]);
+
+
+    const addFolder=()=>{
+     const content= document.getElementsByClassName('File_Page_Add_Button')[0].files;
+     const files=Object.values(content)
+     const folder= files[0].webkitRelativePath.split("/")[0];
+     setRootFolder([...rootFolders,folder]);
+    }
+
+  const Folder=(props)=>{
     return <>
     <div className="FilePage_Folder_Container">
       <div className="FilePage_Folder_Inner_Box">
     <Icon icon="bx:bxs-folder" width="13rem" color="rgb(94 138 189)"/>
-    <h1>Folder 01</h1>
+    <h1>{props.folderName}</h1>
       </div>
     </div>
     </>
@@ -15,18 +25,29 @@ const FilePage = () => {
   return (
     <>
     <div className="FilePage_Container">
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
-      <Folder/>
+      <div className="FilePage_List_of_Folder">
+        {rootFolders.map((value,index)=>{
+          return <Folder folderName={value} key={index}/>
+        })}
+      </div>
+     
+          <input
+            id="image-input"
+            style={{visibility:"hidden",position:"absolute"}}
+            className="File_Page_Add_Button"
+            type="file"
+            placeholder="Image"
+            webkitdirectory="true"
+            multiple
+            onChange={addFolder}
+          />
+     <label
+            htmlFor="image-input"
+            className="FilePage_AddFolder_Button"
+          >
+            <h3>Add Folder</h3>
+        <Icon icon="carbon:add-alt" width="4rem" color="white"/>
+          </label>
       </div>
     </>
   );
