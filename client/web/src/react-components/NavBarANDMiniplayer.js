4f40dc69f81841b7de83ad67e648e79846be09f8
Roman Ojha <razzroman98@gmail.com>
2022-01-24 23:27:44 +0545

    making minplayer responsive

--- client/web/src/react-components/NavBarANDMiniplayer.js
+++ client/web/src/react-components/NavBarANDMiniplayer.js
@@ -16,6 +16,40 @@ const NavBarANDMiniplayer = () => {
   };
   return (
     <>
+      <div className="MVstreamer_MiniPlayer_Container">
+        <div className="MVstreamer_MiniPlayer">
+          <div className="MVstreamer_MiniPlayer_Inner">
+            {/* <Icon
+              className="MVstreamer_MiniPlayer_Icons1"
+              icon="fluent:previous-24-filled"
+              color="#1976D2"
+              width="55px"
+              height="55px"
+              />
+              <img
+              src={PlayButton}
+              style={{
+                width: "100px",
+                height: "100px",
+                marginTop: "23px",
+                cursor: "pointer",
+                zIndex: "3",
+              }}
+              // style={{ width: "100px", height: "100px", cursor: "pointer" }}
+              onClick={() => {
+                audio.pause();
+              }}
+              />
+              <Icon
+              className="MVstreamer_MiniPlayer_Icons3"
+              icon="fluent:next-24-filled"
+              color="DB392C"
+              width="55px"
+              height="55px"
+            /> */}
+          </div>
+        </div>
+      </div>
       <div className="MVstreamer_Navbar">
         <NavLink to="/" style={designActiveLink}>
           <div className="NavBar_HomePage_Outline">
@@ -46,39 +80,6 @@ const NavBarANDMiniplayer = () => {
         <div className="MVstreamer_minPlayer_Space_Dive"></div>
         <div className="MVstreamer_minPlayer_Space_Dive"></div>
         <div className="MVstreamer_minPlayer_Space_Dive"></div>
-        {/* <div className="MVstreamer_MiniPlayer">
-          <div className="MVstreamer_MiniPlayer_TimeStamp"></div>
-          <div className="MVstreamer_MiniPlayer_Inner">
-            <Icon
-              className="MVstreamer_MiniPlayer_Icons1"
-              icon="fluent:previous-24-filled"
-              color="#1976D2"
-              width="55px"
-              height="55px"
-            />
-            <img
-              src={PlayButton}
-              style={{
-                width: "100px",
-                height: "100px",
-                marginTop: "23px",
-                cursor: "pointer",
-                zIndex: "3",
-              }}
-              // style={{ width: "100px", height: "100px", cursor: "pointer" }}
-              onClick={() => {
-                audio.pause();
-              }}
-            />
-            <Icon
-              className="MVstreamer_MiniPlayer_Icons3"
-              icon="fluent:next-24-filled"
-              color="DB392C"
-              width="55px"
-              height="55px"
-            />
-          </div>
-        </div> */}
         <NavLink
           to="/video"
           style={designActiveLink}
