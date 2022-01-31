516689f58767196fa7ac814018a98eb8ae19a745
Roman Ojha <razzroman98@gmail.com>
2022-01-31 15:54:20 +0545

    getting flutter storage permission to access local file

--- client/mobile/lib/screens/file_screen.dart
+++ client/mobile/lib/screens/file_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
 import '../assets/icons/search_icon.dart';
 import '../assets/icons/music_player_icons.dart';
+import 'package:file_finder/file_finder.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FileScreen extends StatefulWidget {
   const FileScreen({Key? key}) : super(key: key);
@@ -10,9 +13,56 @@ class FileScreen extends StatefulWidget {
   _FileScreenState createState() => _FileScreenState();
 }
 
+class FolderNode {
+  final String name;
+  final String type;
+  final List? subFolder;
+  const FolderNode(
+      {required this.name, required this.type, required this.subFolder});
+}
+
+class FileNode {
+  final String name;
+  final Directory fileDirectory;
+  final String type;
+  final String extention;
+  const FileNode(
+      {required this.name,
+      required this.fileDirectory,
+      required this.type,
+      required this.extention});
+}
+
 class _FileScreenState extends State<FileScreen> {
   var _gridView = true;
   Color textColor = const Color.fromRGBO(110, 110, 110, 1);
+  var fileFinder = FileFinder();
+
+  @override
+  initState() async {
+    super.initState();
+    if (Platform.isAndroid) {
+      if (await _requestPermission(Permission.storage)) {
+        var _files = fileFinder.findAll("mp4", sort: false);
+        print(_files);
+      }
+    } else {
+      // This is ios
+    }
+  }
+
+  Future<bool> _requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return true;
+    } else {
+      var result = await permission.request();
+      if (result == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 
   Widget _folder() {
     return InkWell(
@@ -24,19 +74,21 @@ class _FileScreenState extends State<FileScreen> {
         decoration: const BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
-        child: Column(
-          children: const [
-            Icon(
-              Icons.folder,
-              size: 70,
-              color: Color.fromRGBO(94, 138, 189, 1),
-            ),
-            Text(
-              "Folder Name",
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
-            )
-          ],
+        child: GestureDetector(
+          child: Column(
+            children: const [
+              Icon(
+                Icons.folder,
+                size: 70,
+                color: Color.fromRGBO(94, 138, 189, 1),
+              ),
+              Text(
+                "Folder Name",
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
         ),
       ),
     );
